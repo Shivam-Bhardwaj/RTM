103d3b3d4970aef4cefeca977d3d15a75cf717d3
shivam-bhardwaj <curious.antimony@gmail.com>
2025-06-25 21:39:51 -0700

    update 1

--- update_and_commit.ps1
+++ update_and_commit.ps1
@@ -1,21 +1,20 @@
-# update_and_commit.ps1
-# This PowerShell script commits all changes with an incrementing update number as the commit message.
+# PowerShell script to commit and push with incrementing update number
 
-$counterFile = ".git/.update_counter"
+$counterFile = ".update_counter"
 if (!(Test-Path $counterFile)) {
     Set-Content $counterFile 1
 }
 $updateNumber = Get-Content $counterFile
 
 # Stage all changes
- git add .
+git add .
 
 # Commit with incrementing message
- git commit -m "update $updateNumber"
+git commit -m "update $updateNumber"
 
 # Increment and save the counter
 $newNumber = [int]$updateNumber + 1
 Set-Content $counterFile $newNumber
 
 # Push to the current branch
- git push
+git push
